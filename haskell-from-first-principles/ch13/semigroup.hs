5b6ec53b724941628a69ece7058d3fdf94c9d1d6
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-10-15 19:50:03 +0300

    * more semigroup examples

--- haskell-from-first-principles/ch13/semigroup.hs
+++ haskell-from-first-principles/ch13/semigroup.hs
@@ -22,3 +22,29 @@ instance Semigroup (Or a b) where
   Fst f <> Snd s = Snd s
   Snd s <> _ = Snd s
   
+newtype Combine a b = Combine { unCombine :: (a -> b) }
+instance (Semigroup b) => Semigroup (Combine a b) where
+  Combine f <> Combine g = Combine $ \n -> f n <> g n
+
+newtype Comp a = Comp { unComp :: a -> a}
+instance Semigroup (Comp a) where
+  Comp f <> Comp g = Comp $ f . g
+  
+data Validation a b = Failure a | Success b deriving (Eq, Show)
+instance Semigroup a => Semigroup (Validation a b) where
+  Success s1 <> Success s2 = Success s2
+  Success s <> Failure f = Failure f
+  Failure f <> Success s = Failure f
+  Failure f1 <> Failure f2 = Failure $ f1 <> f2
+  
+newtype AccumulateRight a b = AccumulateRight (Validation a b) deriving (Eq, Show)
+instance Semigroup b => Semigroup (AccumulateRight a b) where
+  AccumulateRight (Success s1) <> AccumulateRight (Success s2) =
+    AccumulateRight (Success $ s1 <> s2)
+  AccumulateRight (Success s) <> AccumulateRight (Failure f) =
+    AccumulateRight (Success s)
+  AccumulateRight (Failure f) <> AccumulateRight (Success s) =
+    AccumulateRight (Success s)
+  AccumulateRight (Failure f1) <> AccumulateRight (Failure f2) =
+    AccumulateRight (Failure f2)
+    
