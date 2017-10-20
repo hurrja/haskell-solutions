763d7225844cc0b8388e77848ca20e08e4752736
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-10-20 18:26:02 +0300

    * more concise using semigroup and monoid to fullest

--- haskell-from-first-principles/ch17/ziplist.hs
+++ haskell-from-first-principles/ch17/ziplist.hs
@@ -1,12 +1,15 @@
 import Control.Applicative
-import Data.Monoid ((<>))
+import Data.Semigroup
+import Data.Monoid (mappend)
 
 data List a = Nil | Cons a (List a) deriving (Eq, Show)
+instance Semigroup (List a) where
+  Nil <> l = l
+  l <> Nil = l
+  (Cons x xs) <> l = Cons x (xs <> l)
 instance Monoid (List a) where
   mempty = Nil
-  mappend Nil l = l
-  mappend l Nil = l
-  mappend (Cons x xs) l = Cons x (xs <> l)
+  mappend = (<>)
 instance Functor List where
   fmap _ Nil = Nil
   fmap f (Cons x xs) = Cons (f x) (fmap f xs)
@@ -14,7 +17,7 @@ instance Applicative List where
   pure x = Cons x Nil
   Nil <*> _ = Nil
   _ <*> Nil = Nil
-  (Cons f fs) <*> (Cons x xs) =  Cons (f x) ((f <$> xs) `mappend` (fs <*> (Cons x xs)))
+  (Cons f fs) <*> xs =  (f <$> xs) <> (fs <*> xs)
 
 functions = Cons (+1) (Cons (*2) Nil)
 values = Cons 1 (Cons 2 Nil)
