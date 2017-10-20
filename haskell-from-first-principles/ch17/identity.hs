4f88282606297019236d7f1ee93af4aa9198a212
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-10-20 13:10:17 +0300

    * applicative for identity

---
+++ haskell-from-first-principles/ch17/identity.hs
@@ -0,0 +1,9 @@
+newtype Identity a = Identity a deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity x) = Identity $ f x
+
+instance Applicative Identity where
+  pure f = Identity f
+  (Identity f) <*> (Identity x) = Identity $ f x
+  
