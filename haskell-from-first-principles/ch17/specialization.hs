ff6d88db408688c985dab597b052e8531d982a29
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-10-29 07:33:46 +0200

    * last bits including understanding ((->) a) applicative

--- haskell-from-first-principles/ch17/specialization.hs
+++ haskell-from-first-principles/ch17/specialization.hs
@@ -10,3 +10,18 @@ w = (<*>)
 
 u :: Monoid a => b -> (a, b)
 u = pure
+v :: Monoid a => (a, b -> c) -> (a, b) -> (a, c)
+v = (<*>)
+
+f :: b -> a -> b
+f = pure
+g :: (a -> b -> c) -> (a -> b) -> (a -> c)
+g = (<*>)
+
+-- it then turns out that for this applicative
+-- (f <*> g <*> h <*> v) z = f z (g z) (h z) (v z)
+-- see
+-- https://stackoverflow.com/questions/46992307/an-intuitive-idea-of-the-arrow-operator-applicative
+
+-- a small test
+t = ((\x y z w -> [x, y, z, w]) <*> (1+) <*> (2+) <*> (3+)) 4
