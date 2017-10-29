6afe7c8af0a1d4c90ef8700527aa242f3f27dce5
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-10-29 08:02:58 +0200

    * combinations using liftA3

---
+++ haskell-from-first-principles/ch17/combinations.hs
@@ -0,0 +1,9 @@
+import Control.Applicative (liftA3)
+
+stops, vowels :: String
+stops = "pbtdkg"
+vowels = "aeiou"
+
+combos :: [a] -> [b] -> [c] -> [(a, b, c)]
+combos = liftA3 (\x y z -> (x, y, z))
+
