25d2a293d69c8a82eb6cdf2d250930df8edab9dd
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-10-31 20:28:10 +0200

    * rest of instances sans tests

--- haskell-from-first-principles/ch18/monads.hs
+++ haskell-from-first-principles/ch18/monads.hs
@@ -1,3 +1,5 @@
+import Data.Semigroup
+
 data Nope a = Nope deriving Show
 
 instance Functor Nope where
@@ -12,6 +14,7 @@ instance Monad Nope where
   Nope >>= _ = Nope
 
 --
+
 data DifEither b a = Lefty a | Righty b deriving Show
 
 instance Functor (DifEither b) where
@@ -28,3 +31,45 @@ instance Monad (DifEither b) where
   return = pure
   (Lefty x) >>= f = f x
   (Righty x) >>= _ = Righty x
+
+--
+
+newtype Identity a = Identity a deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity x) = Identity $ f x
+
+instance Applicative Identity where
+  pure = Identity
+  (Identity f) <*> (Identity x) = Identity $ f x
+
+instance Monad Identity where
+  return = pure
+  (Identity x) >>= f = f x
+
+--
+
+data List a = Nil | Cons a (List a) deriving Show
+
+instance Semigroup (List a) where
+  Nil <> l = l
+  l <> Nil = l
+  (Cons x xs) <> l = Cons x (xs <> l)
+instance Monoid (List a) where
+  mempty = Nil
+  mappend = (<>)
+
+instance Functor List where
+  fmap _ Nil = Nil
+  fmap f (Cons x xs) = Cons (f x) (fmap f xs)
+
+instance Applicative List where
+  pure x = Cons x Nil
+  Nil <*> _ = Nil
+  _ <*> Nil = Nil
+  (Cons f fs) <*> xs =  (f <$> xs) <> (fs <*> xs)
+
+instance Monad List where
+  return = pure
+  Nil >>= _ = Nil
+  (Cons x xs) >>= f = (f x) <> (xs >>= f)
