5fb4fd4842581a8a4c54fdafdf193a15834e6f02
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-11-01 21:16:24 +0200

    * some function exercises (last incomplete)

---
+++ haskell-from-first-principles/ch18/functions.hs
@@ -0,0 +1,17 @@
+import Control.Applicative (liftA2)
+
+join' :: Monad m => m (m a) -> m a
+join' = flip (>>=) id
+
+l1 :: Monad m => (a -> b) -> m a -> m b
+l1 = fmap
+
+l2 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
+l2 = liftA2
+
+a :: Monad m => m a -> m (a -> b) -> m b
+a = flip (<*>)
+
+meh :: Monad m => [a] -> (a -> m b) -> m [b]
+meh [] _ = pure []
+meh (x:xs) f =  ((:) (f x)) =<< meh xs
