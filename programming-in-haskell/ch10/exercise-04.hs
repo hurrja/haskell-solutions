d0f0f813dde4bc55a3c2f73a5f70d299399e47df
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-11-04 12:18:11 +0200

    * my first (shaky) real IO

---
+++ programming-in-haskell/ch10/exercise-04.hs
@@ -0,0 +1,23 @@
+adder :: IO ()
+adder = readCount >>= readNumbers >>= writeSum
+  where
+    readCount :: IO Int
+    readCount = do
+      putStr "How many numbers? "
+      countStr <- getLine
+      pure $ (read countStr :: Int)
+      
+    readNumbers :: Int -> IO [Int]
+    readNumbers 0 = pure []
+    readNumbers n = do
+      numStr <- getLine
+      pure (read numStr :: Int) >>= (\num -> fmap ((:) num) $ readNumbers $ n - 1)
+
+    writeSum :: [Int] -> IO ()
+    writeSum xs = do
+      putStr "The total is "
+      putStrLn $ show $ sum xs
+    
+     
+      
+    
