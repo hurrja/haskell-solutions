45a695744f81afd6cb51797ca99b97953c9b992f
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-11-05 13:08:53 +0200

    * simplified a lot

--- programming-in-haskell/ch10/exercise-05.hs
+++ programming-in-haskell/ch10/exercise-05.hs
@@ -1,21 +1,14 @@
 adder :: IO ()
-adder = readCount >>= readNumbers  >>= writeSum
+adder = readCount >>= readNumbers >>= writeSum
   where
+    readNumber :: IO Int
+    readNumber = getLine >>= (\s -> pure (read s :: Int))
+
     readCount :: IO Int
-    readCount = do
-      putStr "How many numbers? "
-      countStr <- getLine
-      pure $ (read countStr :: Int)
+    readCount = putStr "How many numbers? " >> readNumber
       
     readNumbers :: Int -> IO [Int]
-    readNumbers = sequence . go
-      where
-        go :: Int -> [IO Int]
-        go 0 = []
-        go n = (getLine >>= (\s -> pure (read s :: Int))) : (go $ n - 1)
+    readNumbers n = sequence $ replicate n readNumber
 
     writeSum :: [Int] -> IO ()
-    writeSum xs = do
-      putStr "The total is "
-      putStrLn $ show $ sum xs
-      
+    writeSum xs = putStr "The total is " >> (putStrLn $ show $ sum xs)
