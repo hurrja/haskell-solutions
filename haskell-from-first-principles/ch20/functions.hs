9e7b1509e6bd5e0f8fd49891069290585271836e
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-11-10 19:46:16 +0200

    * elem

--- haskell-from-first-principles/ch20/functions.hs
+++ haskell-from-first-principles/ch20/functions.hs
@@ -5,3 +5,6 @@ sum' = getSum . foldMap Sum
 
 product' :: (Foldable t, Num a) => t a -> a
 product' = getProduct . foldMap Product
+
+elem' :: (Foldable t, Eq a) => a -> t a -> Bool
+elem' x = foldr (flip (||) . (x ==)) False
