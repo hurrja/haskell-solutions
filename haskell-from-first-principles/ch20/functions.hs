f55f394b95cf85b57ac234fc46c07693f76e1ff2
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-11-11 17:11:05 +0200

    * last folds

--- haskell-from-first-principles/ch20/functions.hs
+++ haskell-from-first-principles/ch20/functions.hs
@@ -33,3 +33,11 @@ null' = foldr (\_ _ -> False) True
 length' :: Foldable t => t a -> Int
 length' = foldr (const (1+)) 0 
 
+toList' :: Foldable t => t a -> [a]
+toList' = foldr (:) []
+
+fold' :: (Foldable t, Monoid m) => t m -> m
+fold' = foldMap id
+
+foldMap' :: (Foldable t, Monoid m) => (a -> m) -> t a -> m
+foldMap' f = foldr (mappend . f) mempty
