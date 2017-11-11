aab994cc2ab0d03ea23f38efcded3187e65c64be
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-11-11 16:56:22 +0200

    * finally a point- and lambda-free length'

--- haskell-from-first-principles/ch20/functions.hs
+++ haskell-from-first-principles/ch20/functions.hs
@@ -8,3 +8,28 @@ product' = getProduct . foldMap Product
 
 elem' :: (Foldable t, Eq a) => a -> t a -> Bool
 elem' x = foldr (flip (||) . (x ==)) False
+
+minimum' :: (Foldable t, Ord a) => t a -> a
+minimum' xs = case foldr f Nothing xs of
+  Just x -> x
+  Nothing -> error "issue"
+  where
+    f :: Ord a => a -> Maybe a -> Maybe a
+    f x Nothing = Just x
+    f x (Just y) = Just $ min x y
+
+maximum' :: (Foldable t, Ord a) => t a -> a
+maximum' xs = case foldr f Nothing xs of
+  Just x -> x
+  Nothing -> error "issue"
+  where
+    f :: Ord a => a -> Maybe a -> Maybe a
+    f x Nothing = Just x
+    f x (Just y) = Just $ max x y
+
+null' :: Foldable t => t a -> Bool
+null' = foldr (\_ _ -> False) True
+
+length' :: Foldable t => t a -> Int
+length' = foldr (const (1+)) 0 
+
