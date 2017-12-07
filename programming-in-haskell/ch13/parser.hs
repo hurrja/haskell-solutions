9165b20203460d8db5297885023d0d24ba932768
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-12-07 20:48:30 +0200

    * incomplete applicative instance

--- programming-in-haskell/ch13/parser.hs
+++ programming-in-haskell/ch13/parser.hs
@@ -5,3 +5,9 @@ newtype Parser a = P { parse :: String -> Maybe (a, String) }
 instance Functor Parser where
   fmap f (P p) = P (\s -> fmap (\(v, r) -> (f v, r)) $ p s)
 
+instance Applicative Parser where
+  pure v = P (\s -> Just (v, s))
+  (P pf) <*> (P p) = P (\s -> fmap (\(v, r) -> case pf r of
+                                                Nothing -> Nothing
+                                                Just (f, r2) -> Just (f v, r2)) $ p s)
+                          
