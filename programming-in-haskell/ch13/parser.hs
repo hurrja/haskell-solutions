b2317439d8d0a6e130248b802117e2371aeca731
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-12-08 15:07:32 +0200

    * adding parsers * adding alternatives (still not working properly, though) * cleaning up code

--- programming-in-haskell/ch13/parser.hs
+++ programming-in-haskell/ch13/parser.hs
@@ -1,4 +1,5 @@
 module Parser where
+import Control.Applicative
 
 newtype Parser a = P { parse :: String -> Maybe (a, String) }
 
@@ -6,7 +7,7 @@ instance Functor Parser where
   fmap f (P p) = P (\s -> fmap (\(v, r) -> (f v, r)) $ p s)
 
 instance Applicative Parser where
-  pure v = P (\s -> Just (v, s))
+  pure v = P $ \s -> Just (v, s)
   (P pf) <*> (P p) = P (\s -> case p s of
                                 Nothing -> Nothing
                                 Just (v, r) -> case pf r of
@@ -19,11 +20,33 @@ instance Monad Parser where
                       Nothing -> Nothing
                       Just (v, r) -> parse (f v) $ r)
                           
+instance Alternative Parser where
+  empty = P $ \_ -> Nothing
+  (P pa) <|> (P pb) = P $ \s -> pa s <|> pb s
+
 empty :: Parser Bool
-empty = P $ (\s -> if null s then Just (True, s) else Nothing)
+empty = P $ \s -> if null s then Just (True, s) else Nothing
              
 accept :: (Char -> Bool) -> Parser Char
-accept p = P $ (\s -> case s of
-                        [] -> Nothing
-                        (c:cs) -> if p c then Just (c, cs) else Nothing)
+accept p = P (\s -> case s of
+                      [] -> Nothing
+                      (c:cs) -> if p c then Just (c, cs) else Nothing)
 
+comma :: Parser Char
+comma = accept (== ',')
+
+whitespace :: Parser Char
+whitespace = accept $ \c -> (c == '\t') || (c == ' ')
+
+newline :: Parser Char
+newline = accept (== '\n')
+
+doubleQuote :: Parser Char
+doubleQuote = accept (== '"')
+
+data CSVItem = CSVString String | CSVInteger Integer
+csvRow :: Parser [CSVItem]
+csvRow = undefined
+
+csvParser :: Parser [[CSVItem]]
+csvParser = undefined
