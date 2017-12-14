8dc22860289915dca9b8cf2540ffa59f32c48319
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-12-14 20:05:07 +0200

    * compose type (working on applicative)

--- haskell-from-first-principles/ch25/type-functions.hs
+++ haskell-from-first-principles/ch25/type-functions.hs
@@ -1,3 +1,17 @@
+{-# LANGUAGE InstanceSigs #-}
+
 newtype Identity a = Identity { runIdentity :: a }
 
 newtype Compose f g a = Compose { getCompose :: f (g a) } deriving (Eq, Show)
+
+newtype Comp f g a = Comp (f (g a)) -- alternative form, no named field
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  fmap h (Compose fga) = Compose $ (fmap . fmap) h fga
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure :: a -> Compose f g a
+  pure x = Compose $ pure $ pure x
+  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  (<*>) = undefined
+  -- (Compose fgh) <*> (Compose fgx) = Compose $ (<*>) ((<*>) fgh) fgx
