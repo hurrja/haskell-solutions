50217bfa99f4202b01d5ea9b16b90b179c3204d0
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-12-18 19:58:54 +0200

    * applicative instance for compose

--- haskell-from-first-principles/ch25/type-functions.hs
+++ haskell-from-first-principles/ch25/type-functions.hs
@@ -13,5 +13,4 @@ instance (Applicative f, Applicative g) => Applicative (Compose f g) where
   pure :: a -> Compose f g a
   pure x = Compose $ pure $ pure x
   (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
-  (<*>) = undefined
-  -- (Compose fgh) <*> (Compose fgx) = Compose $ (<*>) ((<*>) fgh) fgx
+  (Compose fgh) <*> (Compose fgx) = Compose $ (fmap (<*>) fgh) <*> fgx
