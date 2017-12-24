eb6146e2239005cdb12255abe340519cc36767b4
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-12-24 14:48:34 +0200

    * my first version of EitherT (that typechecks)

---
+++ haskell-from-first-principles/ch26/eithert.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE InstanceSigs #-}
+
+newtype EitherT e m a = EitherT { runEitherT :: m (Either e a) }
+
+instance Functor m => Functor (EitherT e m) where
+  fmap f (EitherT ema) = EitherT $  (fmap . fmap) f ema
+
+instance Applicative m => Applicative (EitherT e m) where
+  pure = EitherT . pure . pure
+  (EitherT emf) <*> (EitherT ema) = EitherT $ (fmap (<*>) emf) <*> ema
+
+instance Monad m => Monad (EitherT e m) where
+  return = pure
+  (>>=) :: EitherT e m a -> (a -> EitherT e m b) -> EitherT e m b
+  (EitherT mea) >>= f = EitherT $ mea >>= (\v -> case fmap f v of
+                                              Left x -> pure $ Left x
+                                              Right x -> runEitherT x)
