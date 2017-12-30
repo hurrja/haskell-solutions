c8f28a8c98e33be09ad25da99f114577f07c8544
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-12-30 17:34:56 +0200

    * monad instance that typechecks

--- haskell-from-first-principles/ch26/readert.hs
+++ haskell-from-first-principles/ch26/readert.hs
@@ -13,5 +13,5 @@ instance Applicative m => Applicative (ReaderT r m) where
 instance Monad m => Monad (ReaderT r m) where
   return = pure
   (>>=) :: ReaderT r m a -> (a -> ReaderT r m b) -> ReaderT r m b
-  (>>=) = undefined
+  (ReaderT rma) >>= f = ReaderT $ \r -> rma r >>= \a -> (runReaderT (f a)) r
   
