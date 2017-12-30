49c5c176db712649429b63d87f1fc71b6736f9a5
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-12-30 08:42:27 +0200

    * applicative instance

--- haskell-from-first-principles/ch26/readert.hs
+++ haskell-from-first-principles/ch26/readert.hs
@@ -5,5 +5,8 @@ newtype ReaderT r m a = ReaderT { runReaderT :: r -> m a }
 instance Functor m => Functor (ReaderT r m) where
   fmap f (ReaderT rma) = ReaderT $ (fmap . fmap) f rma
 
--- instance Applicative m => Applicative (ReaderT r m) where
+instance Applicative m => Applicative (ReaderT r m) where
+  pure = ReaderT . pure . pure
+  (<*>) :: ReaderT r m (a -> b) -> ReaderT r m a -> ReaderT r m b
+  (ReaderT rmab) <*> (ReaderT rma) = ReaderT $ (fmap (<*>) rmab) <*> rma
   
