8d8677a4b4469a953d1a0f24732450eaddc400a3
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-12-31 16:05:34 +0200

    * somewhat uniform parentheses

--- haskell-from-first-principles/ch26/embedded.hs
+++ haskell-from-first-principles/ch26/embedded.hs
@@ -4,11 +4,11 @@ import Control.Monad.Trans.Reader
 
 -- the actual exercise
 embedded :: MaybeT (ExceptT String (ReaderT () IO)) Int
-embedded = MaybeT $ ExceptT $ ReaderT (const (pure (Right (Just 1))))
+embedded = MaybeT . ExceptT . ReaderT $ const (pure $ Right (Just 1))
 
 -- testing with simpler version
 emb :: (ExceptT String (ReaderT () IO)) Int
-emb = ExceptT (ReaderT (const (pure $ Right 1)))
+emb = ExceptT . ReaderT $ const (pure $ Right 1)
 
 -- testing reader
 bar :: ReaderT String (MaybeT []) Int
@@ -19,12 +19,11 @@ foo = ReaderT $ const (MaybeT $ [Just 1])
 
 -- more testing with monad transform combos
 g :: MaybeT (MaybeT []) Int
-g = MaybeT (MaybeT ([Just (Just 1)]))
+g = MaybeT . MaybeT $ [Just (Just 1)]
 
 h :: (ExceptT String (MaybeT [])) Int
-h = ExceptT $ MaybeT $ [Just (Right 1)]
-
+h = ExceptT . MaybeT $ [Just (Right 1)]
 
 t :: MaybeT (ReaderT String []) Int
-t = MaybeT (ReaderT $ const ([Just 1]))
+t = MaybeT . ReaderT $ const ([Just 1])
 
