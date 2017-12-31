39f375c1c38e5efb0db980ffe13c4637d91828e7
Jarmo Hurri <jarmo.hurri@iki.fi>
2017-12-31 19:55:59 +0200

    * added comments about the point of this ex

--- haskell-from-first-principles/ch26/embedded.hs
+++ haskell-from-first-principles/ch26/embedded.hs
@@ -2,9 +2,15 @@ import Control.Monad.Trans.Except
 import Control.Monad.Trans.Maybe
 import Control.Monad.Trans.Reader
 
+-- the most important thing to take from this exercise is the fact
+-- that the structural order of monad transformers is reversed from
+-- the lexical one except for ReaderT
+
 -- the actual exercise
 embedded :: MaybeT (ExceptT String (ReaderT () IO)) Int
 embedded = MaybeT . ExceptT . ReaderT $ const (pure $ Right (Just 1))
+-- note lexical order: maybe - except - reader - io
+-- structural order reader - io - except - maybe
 
 -- testing with simpler version
 emb :: (ExceptT String (ReaderT () IO)) Int
@@ -14,8 +20,8 @@ emb = ExceptT . ReaderT $ const (pure $ Right 1)
 bar :: ReaderT String (MaybeT []) Int
 bar = pure 1
 
-foo :: ReaderT String (MaybeT []) Int
-foo = ReaderT $ const (MaybeT $ [Just 1])
+bar' :: ReaderT String (MaybeT []) Int
+bar' = ReaderT $ const (MaybeT $ [Just 1])
 
 -- more testing with monad transform combos
 g :: MaybeT (MaybeT []) Int
